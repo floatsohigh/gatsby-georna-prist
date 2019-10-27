548a4f5451eec7492e557d5d393a35dbfb7c8c1f
floatsohigh <hello@float.so>
2019-10-27 10:57:18 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
     {
       resolve: "gatsby-source-prismic-graphql",
       options: {
-        repositoryName: "GEORNA", // (REQUIRED, replace with your own)
+        repositoryName: "prist", // (REQUIRED, replace with your own)
         linkResolver: () => post => `/${post.uid}`,
       },
     },
